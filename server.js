b17083f5beb0d36c13c905f19edb66c988e52db4
Paul Yi <pauljsyi@gmail.com>
2023-09-18 03:20:13 -0600

    retrieve coffee data from default / endpoint

--- server.js
+++ server.js
@@ -3,19 +3,29 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const Coffee = require("./models/coffee.model");
 // const firebase = require("firebase");
 // const firebaseui = require("firebaseui");
 
 app.get("/", async (req, res) => {
-  try {
-    res.json({
-      status: 200,
-      message: "API working",
+  Coffee.find()
+    .then((items) => {
+      console.log("res in findAllCoffee: ", items);
+      res.status(200).json(items);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("AN ERROR OCCURED", err);
     });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send("Server Errorrrrr");
-  }
+  // try {
+  //   res.json({
+  //     status: 200,
+  //     message: "API working",
+  //   });
+  // } catch (error) {
+  //   console.error(error);
+  //   return res.status(500).send("Server Errorrrrr");
+  // }
 });
 // JWT
 
@@ -54,7 +64,6 @@ app.use(express.json(), express.urlencoded({ extended: true }));
 const AllUserRoutes = require("./routes/user.routes");
 const AllCoffeeRoutes = require("./routes/coffee.routes");
 AllCoffeeRoutes(app);
-
 AllUserRoutes(app);
 
 // set up multer for storing uploaded files
